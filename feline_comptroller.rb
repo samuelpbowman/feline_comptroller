301b35891115e0808de4711c7483402a08defa31
Samuel Bowman <samuelpbowman@gmail.com>
2017-12-31 04:30:21 -0500

    get album and all images from album

--- feline_comptroller.rb
+++ feline_comptroller.rb
@@ -17,12 +17,24 @@ client_secret = doc.css("api_data imgur client_secret").text
 refresh_token = doc.css("api_data imgur refresh_token").text
 
 wrapper = ImgurWrapper.new(client_id)
+tax_paid = false
 
-r_all = session.subreddit('all')
-r_all.hot.each do |submission|
-  if submission.domain == "imgur.com"
-    album = wrapper.gallery_album(submission.url[18..-1])
+r_all = session.subreddit('testingground4bots')
+r_all.post_stream.each do |post|
+  puts "logging post #{post.title}"
+  if post.domain == "imgur.com"
+    puts post.url[26..-1]
+    album = wrapper.gallery_album(post.url[26..-1])
+    puts album
     if album["success"]
+      images = album["data"]["images"]
+      images.each do |image|
+        if image.description.include? "tax"
+          tax_paid = true
+        end
+      end
+    end
+    if !tax_paid
       # TODO things
     end
   end
--- imgur_wrapper/imgur_wrapper.rb
+++ imgur_wrapper/imgur_wrapper.rb
@@ -8,43 +8,30 @@ class ImgurWrapper
     @client_id = client_id
   end
 =begin
-  def refresher(refresh_token, client_secret)
-    url = URI("https://api.imgur.com/oauth2/token")
-
-    @http = Net::HTTP.new(url.host, url.port)
-    # http.use_ssl = true
-
-    request = Net::HTTP::Post.new(url)
-    request.body = "?refresh_token=#{refresh_token}&client_id=#{@client_id}&client_secret=#{client_secret}&grant_type=refresh_token"
-
-    response = @http.request(request)
-    return JSON.parse(response.read_body)
-  end
-=end
   def album_images(album_hash)
     url = URI("https://api.imgur.com/3/album/#{album_hash}/images")
 
-    @http = Net::HTTP.new(url.host, url.port)
-    @http.use_ssl = true
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
 
     request = Net::HTTP::Get.new(url)
     request["Authorization"] = "Client-ID #{@client_id}"
 
-    response = @http.request(request)
-    return JSON.parse(response.read_body)
+    response = http.request(request)
+    return JSON.parse(response.read_body, :quirks_mode => true)
   end
-
+=end
   def gallery_album(gallery_hash)
     url = URI("https://api.imgur.com/3/gallery/album/#{gallery_hash}")
 
-    @http = Net::HTTP.new(url.host, url.port)
-    @http.use_ssl = true
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
 
     request = Net::HTTP::Get.new(url)
     request["Authorization"] = "Client-ID #{@client_id}"
 
-    response = @http.request(request)
-    return JSON.parse(response.read_body)
+    response = http.request(request)
+    return JSON.parse(response.read_body, :quirks_mode => true)
   end
 
 end
